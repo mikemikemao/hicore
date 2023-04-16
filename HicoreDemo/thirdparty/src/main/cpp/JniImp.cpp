a2f23695aa304d1e43b3644d69d3852bf350a8bf
pirate mike <874754143@qq.com>
2023-04-16 22:18:21 +0800

    add libosip2 library

--- HicoreDemo/thirdparty/src/main/cpp/JniImp.cpp
+++ HicoreDemo/thirdparty/src/main/cpp/JniImp.cpp
@@ -1,13 +1,21 @@
 //
 // Created by pirate
 //
-#include <android/binder_ibinder.h>
 #include "jni.h"
 #include "utils/LogUtil.h"
+
+#include <eXosip2/eX_setup.h>
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_hikvision_ThridParty_native_1helloWorld(JNIEnv *env, jobject thiz) {
     // TODO: implement native_helloWorld()
     LOGCATE("hello world\n");
+    int ret = 0;
+    ret = eXosip_init();
+    if(OSIP_SUCCESS != ret){
+        LOGCATE("eXosip_init failed ret =%d \n",ret);
+        return;
+    }
+    LOGCATD("eXosip_init success \n");
 }
